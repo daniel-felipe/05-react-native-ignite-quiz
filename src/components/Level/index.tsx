ed5e872150914a8c45a0cdf7adbcb52d73e905e2
= <daniell.felipe.2017@gmail.com>
2025-06-04 20:41:11 -0300

    Add color interpolation

--- src/components/Level/index.tsx
+++ src/components/Level/index.tsx
@@ -1,70 +1,89 @@
 import { Pressable, PressableProps, Text } from "react-native";
 import Animated, {
-  useSharedValue,
-  useAnimatedStyle,
-  withTiming,
+	useSharedValue,
+	useAnimatedStyle,
+	withTiming,
+	interpolateColor,
 } from "react-native-reanimated";
 
 import { THEME } from "../../styles/theme";
 import { styles } from "./styles";
+import { useEffect } from "react";
 
 const TYPE_COLORS = {
-  EASY: THEME.COLORS.BRAND_LIGHT,
-  HARD: THEME.COLORS.DANGER_LIGHT,
-  MEDIUM: THEME.COLORS.WARNING_LIGHT,
+	EASY: THEME.COLORS.BRAND_LIGHT,
+	HARD: THEME.COLORS.DANGER_LIGHT,
+	MEDIUM: THEME.COLORS.WARNING_LIGHT,
 };
 
 type Props = PressableProps & {
-  title: string;
-  isChecked?: boolean;
-  type?: keyof typeof TYPE_COLORS;
+	title: string;
+	isChecked?: boolean;
+	type?: keyof typeof TYPE_COLORS;
 };
 
 export function Level({
-  title,
-  type = "EASY",
-  isChecked = false,
-  ...rest
+	title,
+	type = "EASY",
+	isChecked = false,
+	...rest
 }: Props) {
-  const scale = useSharedValue(1);
+	const scale = useSharedValue(1);
+	const checked = useSharedValue(0);
 
-  const COLOR = TYPE_COLORS[type];
+	const COLOR = TYPE_COLORS[type];
 
-  const animatedContainerStyle = useAnimatedStyle(() => {
+	const animatedContainerStyle = useAnimatedStyle(() => {
+		return {
+			transform: [{ scale: scale.value }],
+			backgroundColor: interpolateColor(
+				checked.value,
+				[0, 1],
+				["transparent", COLOR],
+			),
+		};
+	});
+
+  const animatedTextStyle = useAnimatedStyle(() => {
     return {
-      transform: [{ scale: scale.value }],
-    };
+      color: interpolateColor(
+        checked.value,
+        [0, 1],
+        [COLOR, THEME.COLORS.GREY_100]
+      )
+    }
   });
 
-  function handlePressIn() {
-    scale.value = withTiming(1.1);
-  }
+	function handlePressIn() {
+		scale.value = withTiming(1.1);
+	}
 
-  function handlePressOut() {
-    scale.value = withTiming(1);
-  }
+	function handlePressOut() {
+		scale.value = withTiming(1);
+	}
 
-  return (
-    <Pressable onPressIn={handlePressIn} onPressOut={handlePressOut} {...rest}>
-      <Animated.View
-        style={[
-          styles.container,
-          animatedContainerStyle,
-          {
-            borderColor: COLOR,
-            backgroundColor: isChecked ? COLOR : "transparent",
-          },
-        ]}
-      >
-        <Text
-          style={[
-            styles.title,
-            { color: isChecked ? THEME.COLORS.GREY_100 : COLOR },
-          ]}
-        >
-          {title}
-        </Text>
-      </Animated.View>
-    </Pressable>
-  );
+	useEffect(() => {
+		checked.value = withTiming(isChecked ? 1 : 0);
+	}, [isChecked]);
+
+	return (
+		<Pressable onPressIn={handlePressIn} onPressOut={handlePressOut} {...rest}>
+			<Animated.View
+				style={[
+					styles.container,
+					{ borderColor: COLOR },
+					animatedContainerStyle,
+				]}
+			>
+				<Animated.Text
+					style={[
+						styles.title,
+            animatedTextStyle,
+					]}
+				>
+					{title}
+				</Animated.Text>
+			</Animated.View>
+		</Pressable>
+	);
 }
