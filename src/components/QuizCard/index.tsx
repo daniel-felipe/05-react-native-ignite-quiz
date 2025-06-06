9696b7d5f933417a0037aad03ac8789dd35b70a2
= <daniell.felipe.2017@gmail.com>
2025-06-06 10:53:56 -0300

    Add fade in animation to Home cards

--- src/components/QuizCard/index.tsx
+++ src/components/QuizCard/index.tsx
@@ -1,34 +1,43 @@
-import { TouchableOpacity, TouchableOpacityProps, Text, View } from 'react-native';
+import {
+	TouchableOpacity,
+	TouchableOpacityProps,
+	Text,
+	View,
+} from "react-native";
 
-import { styles } from './styles';
-import { THEME } from '../../styles/theme';
+import { styles } from "./styles";
+import { THEME } from "../../styles/theme";
 
-import { LevelBars } from '../LevelBars';
-import { QUIZZES } from '../../data/quizzes';
+import { LevelBars } from "../LevelBars";
+import { QUIZZES } from "../../data/quizzes";
+import Animated, { FadeInUp } from "react-native-reanimated";
 
 type Props = TouchableOpacityProps & {
-  data: typeof QUIZZES[0];
-}
+	data: (typeof QUIZZES)[0];
+	index: number;
+};
 
-export function QuizCard({ data, ...rest }: Props) {
-  const Icon = data.svg;
+const TouchableOpacityAnimated =
+	Animated.createAnimatedComponent(TouchableOpacity);
 
-  return (
-    <TouchableOpacity
-      style={styles.container}
-      {...rest}
-    >
-      <View style={styles.header}>
-        <View style={styles.iconContainer}>
-          {Icon && <Icon size={24} color={THEME.COLORS.GREY_100} />}
-        </View>
+export function QuizCard({ data, index, ...rest }: Props) {
+	const Icon = data.svg;
 
-        <LevelBars level={data.level} />
-      </View>
+	return (
+		<TouchableOpacityAnimated
+			entering={FadeInUp.delay(index * 100)}
+			style={styles.container}
+			{...rest}
+		>
+			<View style={styles.header}>
+				<View style={styles.iconContainer}>
+					{Icon && <Icon size={24} color={THEME.COLORS.GREY_100} />}
+				</View>
 
-      <Text style={styles.title}>
-        {data.title}
-      </Text>
-    </TouchableOpacity>
-  );
+				<LevelBars level={data.level} />
+			</View>
+
+			<Text style={styles.title}>{data.title}</Text>
+		</TouchableOpacityAnimated>
+	);
 }
