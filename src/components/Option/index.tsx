4bfb05b6dc4bcab8bec54bda386ab108fbbd3623
= <daniell.felipe.2017@gmail.com>
2025-06-14 12:42:03 -0300

    add animation on quiz options with skia

--- src/components/Option/index.tsx
+++ src/components/Option/index.tsx
@@ -1,19 +1,52 @@
 import { Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
 
 import { styles } from './styles';
+import { BlurMask, Canvas, Path, runTiming, Skia, useValue } from '@shopify/react-native-skia';
+import { THEME } from '../../styles/theme';
+import { useEffect } from 'react';
 
 type Props = TouchableOpacityProps & {
   checked: boolean;
   title: string;
 };
 
+const CHECK_SIZE = 28;
+const CHECK_STROKE = 2;
+const CIRCLE_RADIUS = (CHECK_SIZE - CHECK_STROKE) / 2;
+
 export function Option({ checked, title, ...rest }: Props) {
+  const percentage = useValue(0);
+
+  const path = Skia.Path.Make();
+  path.addCircle(CHECK_SIZE, CHECK_SIZE, CIRCLE_RADIUS);
+
+  useEffect(() => {
+    runTiming(percentage, checked ? 1 : 0, { duration: 700 });
+  }, [checked]);
+
   return (
-    <TouchableOpacity
-      style={[styles.container, checked && styles.checked]}
-      {...rest}
-    >
+    <TouchableOpacity style={[styles.container, checked && styles.checked]} {...rest}>
       <Text style={styles.title}>{title}</Text>
+
+      <Canvas style={{ height: CHECK_SIZE * 2, width: CHECK_SIZE * 2 }}>
+        <Path
+          path={path}
+          color={THEME.COLORS.GREY_500}
+          style="stroke"
+          strokeWidth={CHECK_STROKE}
+        />
+
+        <Path
+          path={path}
+          color={THEME.COLORS.BRAND_LIGHT}
+          style="stroke"
+          strokeWidth={CHECK_STROKE}
+          start={0}
+          end={percentage}
+        >
+          <BlurMask blur={1} style="solid" />
+        </Path>
+      </Canvas>
     </TouchableOpacity>
   );
 }
